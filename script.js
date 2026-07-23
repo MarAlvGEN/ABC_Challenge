5ebb275f95fbb0e5fe3cf66ba85a8f0191c76f73
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-07-23 12:20:03 -0500

    add cards AB with style and js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo");
+const gallery = document.getElementById('gallery');
+
+gallery.addEventListener('click', (event) => {
+  const card = event.target.closest('.card');
+  if (!card) return;
+
+  voltear(card);
+});
+
+function voltear(card) {
+  const yaEstabaVolteada = card.classList.contains('volteada');
+  card.classList.toggle('volteada');
+}
